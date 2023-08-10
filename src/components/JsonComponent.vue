9d2954fcece0be83afb0ea21c45ddd98fb51c3b7
Zubair Ahmad <zaa78692@gmail.com>
2023-08-10 14:57:50 +0530

    Create JsonComponent.vue

---
+++ src/components/JsonComponent.vue
@@ -0,0 +1,11 @@
+  <template>
+  <div>
+    <h1>RenderCompoent</h1>
+    <div class="RenderCompoent">
+      <RenderCompoent msg="helloWorlrd" />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import RenderCompoent from '@/utils/renderFunction';
+</script>
